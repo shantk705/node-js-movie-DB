42c47331c370d529ab686646b7c665fb45bb5edf
shant-k <shantk650@gmail.com>
2023-01-09 07:57:44 +0200

    .

--- movie-database/index.js
+++ movie-database/index.js
@@ -3,7 +3,19 @@ const app = express()
 const port = process.env.port || 4000;
 
 app.get('/a', (req,res)=>{
+    
     res.send("ok");
+})
+app.get('/test', (req,res)=>{
+    
+    res.send({status:200, message:"ok"});
+})
+app.get('/test', (req,res)=>{
+    
+    res.send(app.get('/time', (req,res)=>{
+    
+        res.send( {status:200, message:<TIME/>});
+    }));
 })
 
 app.listen(port,() =>{
